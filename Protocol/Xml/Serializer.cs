0743e1558fd5212c1497bb2b627adca2596a1ee1
Константин Дьяченко <konard@me.com>
2019-08-15 07:55:39 +0300

    Platform.Communication.Protocol.Xml.Serializer moved from Platform.Helpers.Serialization.Xml.

---
+++ Protocol/Xml/Serializer.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Platform.Helpers.Serialization
+{
+    public static class Serializer<T>
+    {
+        public static readonly XmlSerializer Instance = new XmlSerializer(typeof(T));
+
+        public static T FromFile(string path)
+        {
+            using (var stream = File.OpenRead(path))
+            {
+                return (T)Instance.Deserialize(stream);
+            }
+        }
+
+        public static T FromString(string xml)
+        {
+            using (var reader = new StringReader(xml))
+            {
+                return (T)Instance.Deserialize(reader);
+            }
+        }
+
+        public static void ToFile(T @object, string path)
+        {
+            using (var stream = File.OpenWrite(path))
+            {
+                Instance.Serialize(stream, @object);
+            }
+        }
+
+        public static string ToString(T @object)
+        {
+            var sb = new StringBuilder();
+            using (var writer = new StringWriter(sb))
+            {
+                Instance.Serialize(writer, @object);
+            }
+            return sb.ToString();
+        }
+    }
+}
