96d428e09e8237fec3e423e5ba60eecffd10001e
Константин Дьяченко <konard@me.com>
2019-08-16 11:40:52 +0300

    Added tests moved from https://github.com/Konard/LinksPlatform/blob/2f05a8bdb1f1f854cc96596118e69be752a04ded/Platform/Platform.Tests/Helpers/SerializationHelpersTests.cs

---
+++ Platform.Communication.Tests/SerializerTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using Xunit;
+using Platform.Singletons;
+using Platform.Communication.Protocol.Xml;
+
+namespace Platform.Communication.Tests
+{
+    public static class SerializerTests
+    {
+        [Fact]
+        public static void SerializeToFileTest()
+        {
+            var tempFilename = Path.GetTempFileName();
+            Serializer<object>.ToFile(Default<object>.Instance, tempFilename);
+            Assert.Equal(File.ReadAllText(tempFilename), $"<?xml version=\"1.0\"?>{Environment.NewLine}<anyType xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />");
+            File.Delete(tempFilename);
+        }
+
+        [Fact]
+        public static void SerializeAsXmlStringTest()
+        {
+            var serializedObject = Serializer<object>.ToString(Default<object>.Instance);
+            Assert.Equal(serializedObject, $"<?xml version=\"1.0\" encoding=\"utf-16\"?>{Environment.NewLine}<anyType xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />");
+        }
+    }
+}
