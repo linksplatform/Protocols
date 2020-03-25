4c187ad6c7322849fd595f0668c6b42f7a379f3c
Konstantin Diachenko <drakonard@gmail.com>
2020-03-25 16:01:02 +0300

    Platform.Singletons dependency is removed.

--- csharp/Platform.Communication/Protocol/Udp/UdpClientExtensions.cs
+++ csharp/Platform.Communication/Protocol/Udp/UdpClientExtensions.cs
@@ -2,7 +2,6 @@
 using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Text;
-using Platform.Singletons;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
